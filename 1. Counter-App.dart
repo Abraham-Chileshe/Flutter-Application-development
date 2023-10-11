57c1fc977983d6d2044f51fc889dfae62e06f5f7
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2023-10-12 00:33:59 +1000

    Update 1. Counter-App.dart

--- 1. Counter-App.dart
+++ 1. Counter-App.dart
@@ -1,73 +1,87 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+    runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: CounterApp(),
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            home: CounterApp(),
+        );
+    }
 }
 
 class CounterApp extends StatefulWidget {
-  @override
-  _CounterAppState createState() => _CounterAppState();
+    @override
+    _CounterAppState createState() => _CounterAppState();
 }
 
 class _CounterAppState extends State<CounterApp> {
-  int _counter = 0;
+    int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+    void _incrementCounter() {
+        setState(() {
+            _counter++;
+        });
+    }
 
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
+    void _decrementCounter() {
+        setState(() {
+            _counter--;
+        });
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Простой счетчик'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Значение счетчика:',
-              style: TextStyle(fontSize: 20),
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: Text('Простой счетчик'),
+                centerTitle: true,
+                backgroundColor: Colors.pink[800]
             ),
-            Text(
-              '$_counter',
-              style: TextStyle(fontSize: 50),
-            ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ElevatedButton(
-                  onPressed: _incrementCounter,
-                  child: Icon(Icons.add),
+            body: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                        Text(
+                            'Значение счетчика:',
+                            style: TextStyle(
+                                fontSize: 25,
+                            ),
+                        ),
+                        Text(
+                            '$_counter',
+                            style: TextStyle(
+                                fontSize: 50,
+                                fontWeight: FontWeight.bold,
+                            ),
+                        ),
+                        SizedBox(height: 20),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                                ElevatedButton(
+                                    onPressed: _incrementCounter,
+                                    style: ElevatedButton.styleFrom(
+                                        primary: Colors.pink[800], // Background color
+                                    ),
+                                    child: Icon(Icons.add),
+                                ),
+                                ElevatedButton(
+                                    onPressed: _decrementCounter,
+                                    style: ElevatedButton.styleFrom(
+                                        primary: Colors.pink[800], // Background color
+                                    ),
+                                    child: Icon(Icons.remove),
+                                ),
+
+                            ],
+                        ),
+                    ],
                 ),
-                ElevatedButton(
-                  onPressed: _decrementCounter,
-                  child: Icon(Icons.remove),
-                ),
-              ],
             ),
-          ],
-        ),
-      ),
-    );
-  }
+        );
+    }
 }
